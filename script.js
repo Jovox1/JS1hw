e88e6259280c870829a13f6dae222453c7366cb9
Javohir <javohir-ochilov@mail.ru>
2022-11-16 15:38:50 +0500

    JS1hw

---
+++ script.js
@@ -0,0 +1,19 @@
+var str = prompt('Введите свое имя и возраст')
+console.log(str);
+var sloj = prompt('6+3 =')
+var vichit = prompt('6-3 =')
+var umnoj = prompt('6*3 =')
+var del = prompt('6/3 =')
+var ostatok = prompt('7%3 =')
+var YourAnswer = ('Ваш ответ')
+alert('Зайдите в консоль разработчика F12')
+var plus = ('6 + 3 = ' + (6 + 3))
+console.log(plus + '(' + YourAnswer + ' ' + sloj + ')');
+var minus = ('6 - 3 = ' + (6 - 3))
+console.log(minus + '(' + YourAnswer + ' ' + vichit + ')');
+var umnojeniye = ('6 * 3 = ' + (6 * 3))
+console.log(umnojeniye + '(' + YourAnswer + ' ' + umnoj + ')');
+var delenie = ('6 / 3 = ' + (6 / 3))
+console.log(delenie + '(' + YourAnswer + ' ' + del + ')');
+var ost = ('7 % 3 = ' + (7 % 3))
+console.log(ost + '(' + YourAnswer + ' ' + ostatok + ')');
